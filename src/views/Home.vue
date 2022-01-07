6a7b0516a648f89ea8a810401ca9fa82dfbfbba6
rucckrussell <ruckrussell@outlook.com>
2022-01-07 12:48:02 -0600

    h '#' will be ignored, and an empty message aborts the commit.

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,13 @@
 <template>
 <div class="home">
-  <p>¡Hola vue! uwu</p>
+  <h1>¡Hola vue! uwu</h1>
+  <p class="red white--text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic autem facilis fugit natus reprehenderit eaque est totam eligendi nesciunt eveniet! Esse quo, sed tempore perferendis vel fugiat nobis facere voluptates!</p>
+  <p class="pink lighten-4 red--text text--darken-4">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia distinctio explicabo facilis tenetur, minus voluptates pariatur amet odio! Dignissimos reiciendis vero adipisci, repellat est alias quis repudiandae aliquam incidunt facere!</p>
+  <h1 class="display-4">Mensaje grandote</h1>
+  <h1 class="display-1">Mensaje peque</h1>
+  <p class="headline">This is a Headline</p>
+  <p class="subheading">This is a subheading</p>
+  <p class="caption">This is a caption</p>
 </div>
 </template>
 
