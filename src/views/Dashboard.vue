0ce9b4599f5accb560b6630c908adfd6e79f7e75
rucckrussell <ruckrussell@outlook.com>
2022-01-10 15:44:18 -0600

    Grid en v-flex y v-layout (pt 1)

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -3,7 +3,32 @@
   <h1 class="subheading grey--text">Dashboard</h1>
   
   <v-container class="my-5">
-    content
+    
+    <v-layout wrap row>
+      <v-flex xs12 md2>
+        <v-btn outline block class="primary">1</v-btn>
+      </v-flex>
+      <v-flex xs4 md4>
+        <v-btn outline block class="primary">2</v-btn>
+      </v-flex>
+      <v-flex xs4 md4>
+        <v-btn outline block class="primary">2</v-btn>
+      </v-flex>
+      <v-flex xs4 md4>
+        <v-btn outline block class="primary">2</v-btn>
+      </v-flex>
+
+    </v-layout>
+
+    <v-layout row wrap justify-space-around>
+      <v-flex xs4 md6>
+        <v-btn outline block class="success">1</v-btn>
+      </v-flex>
+      <v-flex xs4 md6>
+        <v-btn outline block class="success">1</v-btn>
+      </v-flex>
+    </v-layout>
+
   </v-container>
 
 </div>
