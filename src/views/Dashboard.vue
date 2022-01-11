045e6ac2f2c1f740c6ee5d625575c330cd89e03b
rucckrussell <ruckrussell@outlook.com>
2022-01-11 15:09:28 -0600

    Ordenamiento de los Proyectos

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -3,6 +3,18 @@
   <h1 class="grey--text">Dashboard</h1>
   
   <v-container class="my-5">
+
+    <v-row class="mb-3">
+      <v-btn small text color="grey" @click="sortBy('title')">
+      <v-icon left small >mdi-folder</v-icon>
+      <span class="caption text-lowercase">By project name</span>
+      </v-btn>
+      <v-btn small text color="grey" @click="sortBy('person')">
+      <v-icon left small >mdi-account</v-icon>
+      <span class="caption text-lowercase">By person</span>
+      </v-btn>
+    </v-row>
+
     <v-card flat  v-for="project in projects" :key="project.title">
       <v-row no-gutters :class="`pa-3 project ${project.status}`">
         <v-flex xs12 md6>
@@ -42,8 +54,12 @@
           { title: 'Create a community forum', person: 'Gouken', due: '20th Oct 2018', status: 'overdue', content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt consequuntur eos eligendi illum minima adipisci deleniti, dicta mollitia enim explicabo fugiat quidem ducimus praesentium voluptates porro molestias non sequi animi!'},
         ]
       }
-
     },
+    methods: {
+      sortBy(type){
+        this.projects.sort((a,b) => a[type] < b[type] ? -1 : 1)
+      }
+},
     components: {
     },
   }
