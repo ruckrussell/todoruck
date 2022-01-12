3693b5e8384a3b66e8c7d25af6b998ac470c4f32
ruckrussell <ruckrussell@outlook.com>
2022-01-12 12:38:00 -0600

    Agregar Avatares a vista Teams y Navbar

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -13,6 +13,20 @@
             </v-btn>
         </v-app-bar>
         <v-navigation-drawer v-model="drawer" app class="primary">
+            <v-layout column align-center>
+                <v-flex class="mt-5">
+                    <v-avatar size="100">
+                        <img src="/avatar-1.png" alt="">
+                    </v-avatar>
+                </v-flex>
+            </v-layout>
+            <v-layout column align-center>
+                <v-flex class="">
+                    <p class="white--text subtitle-1 font-weight-medium mt-1">
+                        Ruckrussell
+                    </p>
+                </v-flex>
+            </v-layout>
             <v-list>
                 <v-list-item v-for="link in links" :key="link.text" router :to="link.route">
                     <v-list-item-action>
