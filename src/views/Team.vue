5511e0939183045c05b2b5c195ac856ea1225d74
ruckrussell <ruckrussell@outlook.com>
2022-01-12 12:05:43 -0600

    Tarjetas de Usuarios vista team

--- src/views/Team.vue
+++ src/views/Team.vue
@@ -3,7 +3,27 @@
   <h1 class="grey--text">Team</h1>
   
   <v-container class="my-5">
-    content
+    
+    <v-row>
+      <v-flex xs12 sm6 md4 lg3 v-for="person in team" :key="person.name">
+        <v-card flat class="text-center ma-3">
+          <v-responsive class="pt-4">
+            image goes here
+          </v-responsive>
+          <v-card-text>
+            <div class="subtitle-1 font-weight-medium">{{person.name}}</div>
+            <div class="grey--text">{{person.role}}</div>
+          </v-card-text>
+          <v-card-actions>
+            <v-btn text flat color="grey">
+              <v-icon small left>mdi-message</v-icon>
+              <span>Message</span>
+            </v-btn>
+          </v-card-actions>
+        </v-card>
+      </v-flex>
+    </v-row>
+
   </v-container>
 
 </div>
@@ -13,7 +33,17 @@
 
   export default {
     name: 'Team',
-
+    data() {
+      return {
+        team: [
+          { name: 'The net Ninja', role: 'Web developer' },
+          { name: 'Ryu', role: 'Graphic desginer' },
+          { name: 'Chun Li', role: 'Web developer' },
+          { name: 'Gouken', role: 'Social media maverick' },
+          { name: 'Yoshi', role: 'Sales guru' }
+        ]
+      }
+    },
     components: {
     },
   }
