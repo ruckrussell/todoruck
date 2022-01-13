ee4ace7db6da8e1d21983872cf471792cdf902a5
ruckrussell <ruckrussell@outlook.com>
2022-01-13 11:51:54 -0600

    Popup de añadir proyecto

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -43,6 +43,9 @@
                         Ruckrussell
                     </p>
                 </v-flex>
+                <v-flex class="mt-4 mb-3">
+                    <Popup />
+                </v-flex>
             </v-layout>
             <v-list>
                 <v-list-item v-for="link in links" :key="link.text" router :to="link.route">
@@ -59,7 +62,12 @@
     </nav>
 </template>
 <script>
+
+import Popup from './Popup'
+
+
 export default {
+    components: { Popup },
     data() {
         return{
             drawer: false,
---
+++ src/components/Popup.vue
@@ -0,0 +1,20 @@
+<template>
+    <v-dialog max-width="600px">
+        <template v-slot:activator="{ on, attrs }">
+            <v-btn v-bind="attrs" v-on="on" class="success">
+                <span>Add new project</span>
+            </v-btn>
+        </template>
+        <v-card>
+            <v-card-title>
+                    <h2>Add a new Project</h2>
+            </v-card-title>
+        </v-card>
+    </v-dialog>
+</template>
+
+<script>
+export default {
+
+}
+</script>
